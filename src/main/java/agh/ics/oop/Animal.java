40544ddfddd35b03cf06415728342faf39cd58b0
Mapet <jakubsordyl1@gmail.com>
2021-12-08 18:13:00 +0100

    add Animals constructors logic

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -1,30 +1,68 @@
 package agh.ics.oop;
 
+import java.util.Random;
+
 public class Animal extends MapEntity {
-    public int getEnergy() {
-        return energy;
+
+  // info o child
+  private final Genome genome;
+  private int age = 0;
+  private int energy = 0; // VO?
+  private Direction direction;
+
+  public Animal(Vector2d position, int energy) {
+    this.position = position;
+    this.energy = energy;
+    genome = Genome.Randomize();
+  }
+
+  public Animal(Animal firstParent, Animal secondParent) {
+    Animal[] parents = getOrderedParents(firstParent, secondParent);
+
+    genome = Genome.From(
+              parents[0].genome,
+              parents[1].genome,
+              (float) parents[0].energy / (float) parents[1].energy);
+
+    position = firstParent.position;
+
+    appropriateParentalEnergy(parents);
+  }
+
+  public int getEnergy() {
+    return energy;
+  }
+
+  public void eat(int e) {
+    energy += e;
+  }
+
+  public void move() {
+    age += 1;
+
+    Direction moveDirection = getNextDirection();
+
+    direction = direction.rotateTowards(moveDirection);
+
+    if (moveDirection == Direction.N || moveDirection == Direction.S)
+      position = position.add(direction.toUnitVector());
+  }
+
+  private Animal[] getOrderedParents(Animal first, Animal second) {
+    return (new Random().nextInt(2)) == 1
+        ? new Animal[] {first, second}
+        : new Animal[] {second, first};
+  }
+
+  private void appropriateParentalEnergy(Animal[] parents) {
+    for (Animal parent : parents) {
+      int parentalEnergy = Math.round(parent.energy / 4.0f);
+      parent.energy -= parentalEnergy;
+      energy += parentalEnergy;
     }
+  }
 
-    public void eat(int e) {
-        energy += e;
-    }
-
-    public void move() {
-        Direction moveDirection = getNextDirection();
-
-        direction = direction.rotateTowards(moveDirection);
-
-        if (moveDirection == Direction.N || moveDirection == Direction.S)
-            position = position.add(direction.toUnitVector());
-    }
-
-    Direction getNextDirection() {
-        return Direction.N; // todo
-    }
-
-    // info o child
-    private int energy; // VO?
-    private int age;
-    private Direction direction;
-    private Genome genome;
+  Direction getNextDirection() {
+    return Direction.N; // todo
+  }
 }
